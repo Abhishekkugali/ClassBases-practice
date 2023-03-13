4edfc37f5bebb61775359982d55f5c70cc277583
Abhishekkugali <126229384+Abhishekkugali@users.noreply.github.com>
2023-03-14 00:20:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+
+class App extends Component
+{
+    render(){
+        return(
+            <>
+            <h1>Today is hit man show</h1>
+            </>
+        )
+    }
+}
+
+export default App;
